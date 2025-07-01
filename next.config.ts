4bf2276380154925a0176a66e97c5949fe6083a7
caseydb <casey.dbnz@gmail.com>
2025-07-01 00:31:04 -0400

    .

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Exclude functions directory from Next.js compilation
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({
+        "firebase-functions": "firebase-functions",
+        "firebase-admin": "firebase-admin",
+      });
+    }
+    return config;
+  },
+  // Exclude functions directory from TypeScript checking
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  experimental: {
+    typedRoutes: true,
+  },
 };
 
 export default nextConfig;
