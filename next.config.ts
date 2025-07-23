ab68554d5d0cff6d11ae74c8498090cda08a7dc8
caseydb <casey.dbnz@gmail.com>
2025-07-23 13:04:46 -0400

    v2

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
     FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
     FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
     FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
+    DATABASE_URL: process.env.DATABASE_URL,
   },
   typescript: {
     ignoreBuildErrors: false,
