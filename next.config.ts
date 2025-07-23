18c3f7c754a3566d7cda84a8237aae927a0442d0
caseydb <casey.dbnz@gmail.com>
2025-07-23 12:41:41 -0400

    added next.config for build vars

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = config.externals || [];
-      config.externals.push({
-        "firebase-functions": "firebase-functions",
-        "firebase-admin": "firebase-admin",
-      });
-    }
-    return config;
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+    FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
   },
   typescript: {
     ignoreBuildErrors: false,
